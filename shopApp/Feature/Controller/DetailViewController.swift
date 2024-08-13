587b22d8f2083ca6867832db84f58f639182ce18
cansu <cansue288@gmail.com>
2024-08-13 18:05:42 +0300

    Detay ekranı tasarlandı.

--- shopApp/Feature/Controller/DetailViewController.swift
+++ shopApp/Feature/Controller/DetailViewController.swift
@@ -8,14 +8,32 @@
 import UIKit
 
 final class DetailViewController: UIViewController {
-
+    //MARK: -variables
+    
+    @IBOutlet weak var productImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var cartButton: UIButton!
+    
+    @IBOutlet weak var detailView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         print("hello2")
-
+        productImageView.image = UIImage(named: "ayakkabi")
+        titleLabel.text = "Adidas"
+        priceLabel.text = "100 TL"
+        descriptionLabel.text = "Siyah beyaz ayakkabı"
+        cartButton.layer.cornerRadius = 18
+        detailView.layer.cornerRadius = 18
+        productImageView.layer.cornerRadius = 18
     }
     
+    //MARK: -function
 
  
-
+    @IBAction func cartButtonClicked(_ sender: UIButton) {
+        print("sepete eklendi")
+    }
+    
 }
