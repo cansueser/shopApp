13323a4a5e4dcee3d7ccfd1f9f4ccd5a9a49aa2f
cansu <cansue288@gmail.com>
2024-08-15 19:21:22 +0300

    Detay ekranına yıldız eklendi.

--- shopApp/Feature/Controller/DetailViewController.swift
+++ shopApp/Feature/Controller/DetailViewController.swift
@@ -17,15 +17,24 @@ final class DetailViewController: UIViewController {
     @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var cartButton: UIButton!
     @IBOutlet weak var detailView: UIView!
-    var product : Product?
+    @IBOutlet weak var star1: UIImageView!
+    @IBOutlet weak var star3: UIImageView!
+
+    @IBOutlet weak var star5: UIImageView!
+    @IBOutlet weak var star4: UIImageView!
+    @IBOutlet weak var star2: UIImageView!
     
+    @IBOutlet weak var rateCountLabel: UILabel!
+    var product : Product?
+   
     //MARK: -function
     override func viewDidLoad() {
         super.viewDidLoad()
         print("hello2")
         settingsDetail()
+        getStar()
     }
-
+    
     @IBAction func cartButtonClicked(_ sender: UIButton) {
         print("sepete eklendi")
     }
@@ -36,11 +45,23 @@ final class DetailViewController: UIViewController {
             titleLabel.text = product.title
             priceLabel.text = "\(product.price)$"
             descriptionLabel.text = product.description
+            rateCountLabel.text = "(\(product.rating.count))"
         }
         
         cartButton.layer.cornerRadius = 18
         detailView.layer.cornerRadius = 18
         productImageView.layer.cornerRadius = 18
     }
-    
+    func getStar() {
+        let stars = [star1,star2,star3,star4,star5]
+        var count = 0
+        let rate = Int((product?.rating.rate)!)
+        for star in stars {
+            if count < rate{
+                star?.image = UIImage(systemName: "star.fill")
+            }
+            count += 1
+        }
+    }
 }
+
